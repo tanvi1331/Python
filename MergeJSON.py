7bb3d2d8f806ffd412ea20218f42120f9e99dc21
tanvi1331 <36177420+tanvi1331@users.noreply.github.com>
2018-05-26 14:11:44 -0400

    Update MergeJSON.py

--- MergeJSON.py
+++ MergeJSON.py
@@ -20,17 +20,3 @@ for file in glob.glob('/home/tanvi/Documents/files/Tweets*.json'):
     with open('/home/tanvi/Documents/files/mergedTweetsFile.json', 'w') as f:
         json.dump(glob_data, f, indent=4)
         print('file written')
-#list1= '/home/tanvi/Documents/files/tweets1.json'
-#list2= '/home/tanvi/Documents/files/tweets2.json'
-#
-#with open(list1) as file:
-#    tweets_data1 = json.loads(file.read())
-#
-#with open(list2) as file:
-#    tweets_data2 = json.loads(file.read())
-#
-#res_dict = tweets_data1 + tweets_data2
-#
-#with open('/home/tanvi/Documents/files/mergedfile.json', 'w') as f:
-#    json.dump(res_dict, f, indent=4)
-#    print('file written')
